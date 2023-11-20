2befa0487168b5732028de5d465681e640ced811
Adam Kubanski <akubansa@gmail.com>
2023-11-20 08:40:24 +0100

    modyfy project

--- anki_flashcards_with_gpt4_word.py
+++ anki_flashcards_with_gpt4_word.py
@@ -1,14 +1,11 @@
 import os
 import openai
 import pyperclip
-from dotenv import load_dotenv
-# pip install python-dotenv==0.21.0
+
 
 # Set up OpenAI API key
-load_dotenv()
-
-openai.api_key = os.environ["OPENAI_API_KEY"]
-#openai.api_key = os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+#openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 #------------wgranie klucza------------
 #echo "export OPENAI_API_KEY=''" >> ~/.zshrc
@@ -23,7 +20,7 @@ clipboard_content = pyperclip.paste()
 #clipboard_content = 'consider'
 # Create a conversation with ChatGPT
 messages = [
-    {"role": "system", "content": "you are an English teacher, student Polish. Provide example sentence for using word. State the question::answer in one line"},
+    {"role": "system", "content": "you are an English teacher, student Polish. Przetłumacz słowo z jezyka polskiego na angielski i na odwrot i nic wiecej. State the question::answer in one line"},
     {"role": "user", "content": f"Create anki flashcards with the following text using a format like line question::answer, next line question::answer. state the question;answer in one line, etc... word:{clipboard_content}."}
 ]
 #Utwórz fiszki anki z następującym tekstem tlumacac go na jezyk polski, używając formatu takiego jak pytanie;odpowiedź w następnym wierszu pytanie;odpowiedź itp.{clipboard_content}."}
@@ -41,16 +38,18 @@ generated_flashcards = response["choices"][0]["message"]["content"]
 # Save flashcards to a file
 
 
-with open("/Users/kubansa/project/anki_gpt/flashcards.txt", "a") as f:
+with open("/Users/kubansa/project/anki_gpt/flashcards2.txt", "a") as f:
     f.write(clipboard_content)
     f.write("\n")
 
 
-with open("/Users/kubansa/project/anki_gpt/flashcards.txt", "a") as f:
+with open("/Users/kubansa/project/anki_gpt/flashcards2.txt", "a") as f:
     f.write(generated_flashcards)
     f.write("\n")
     f.write("\n")
 
-print("Flashcards saved to 'flashcards.txt'")
+print("Flashcards saved to 'flashcards2.txt'")
 
+print(clipboard_content)
 
+print(generated_flashcards)
--- test.py
+++
@@ -1,8 +0,0 @@
-with open("flashcards.txt") as file:
-            for row in file.readlines()[1:]:
-                question, answer =  (row.split("::")[0:])
-                notes = "('" + question + "'," + "'"+answer + "')"
-                with open("/Users/kubansa/project/anki_gpt/flashcards2.txt", "a") as f:
-                    f.write(notes)
-
-            
